0ce5a33f46eb8f8dfbb5a26b42c8879c25a85aa6
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-12-27 09:28:06 -0500

    add scroll button to scroll to top of the tweets

---
+++ public/scripts/composer.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    const scrollHeight = $(this).scrollTop();
+    if (scrollHeight > 450) {
+      $("#scroll button").show();
+    } else {
+      $("#scroll button").hide();
+    }
+  });
+  $("#scroll button").click(function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+});
