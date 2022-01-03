206537230489dc7e9d78108e96807e7ec5175299
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-01-03 12:09:17 -0500

    modify scroll button visible condition

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $(window).scroll(function() {
     const scrollHeight = $(this).scrollTop();
-    if (scrollHeight > 450) {
+    const scrollLength = window.innerHeight * 0.5;
+    //do not use absolute values in px as screen sizes can change
+    if (scrollHeight > scrollLength) {
       $("#scroll button").show();
     } else {
       $("#scroll button").hide();
