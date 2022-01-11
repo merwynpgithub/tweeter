8be3074ba6fdea3dfe227a6b9bf3536d27f22b3b
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-01-11 08:16:54 -0500

    change jquery event listener from keyup to input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   //do not use change event
-  $("#tweet-text").keyup(function() {
+  $("#tweet-text").on('input', function() {
     const textLimit = 140 - this.value.length;
     $(".new-tweet div output").html(textLimit);
     if (textLimit < 0) {
